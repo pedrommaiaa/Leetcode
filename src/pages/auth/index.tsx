8112bef363efdc46e3e1eca7a1bd7bf72a767b55
Pedro Maia <pedrommaiaa@protonmail.com>
2024-01-31 19:05:13 -0500

    firebase authentication working

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,15 +1,28 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import AuthModal from '@/components/Modals/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+import { auth } from '@/firebase/firebase';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
 
-type authPageProps = {
-    
-};
+type authPageProps = {};
 
 const authPage:React.FC<authPageProps> = () => {
+    const router = useRouter();
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState(true);
+
+    useEffect(() => {
+        if(user) router.push('/');
+        if(!loading && !user) setPageLoading(false);
+    }, [error, router, loading]);
+
+    if (pageLoading) return null;
+
+
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
         <div className='max-w-7x1 mx-auto'>
             <Navbar />
